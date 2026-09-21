10f8492ab990671a0d3aaef6949d3727f8fcf879
Ignacio Duart <iduartgomez@gmail.com>
2026-09-22 00:10:22 +0200

    fix(evals): enable codex collaboration v2

--- evals/dp-scenarios/src/dp_scenarios/runner/codex_adapter.py
+++ evals/dp-scenarios/src/dp_scenarios/runner/codex_adapter.py
@@ -1273,6 +1273,8 @@ class CodexAdapter:
             "--stdio",
             "--enable",
             "multi_agent",
+            "--enable",
+            "multi_agent_v2",
             "-c",
             'approval_policy="never"',
             "-c",
