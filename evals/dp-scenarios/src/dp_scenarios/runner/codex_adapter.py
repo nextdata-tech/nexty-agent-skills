d08554a4b0eac0ab94bbe1849dbe59bccfa33679
Ignacio Duart <iduartgomez@gmail.com>
2026-09-22 00:19:13 +0200

    revert(evals): disable unproven codex collaboration v2

--- evals/dp-scenarios/src/dp_scenarios/runner/codex_adapter.py
+++ evals/dp-scenarios/src/dp_scenarios/runner/codex_adapter.py
@@ -1030,8 +1030,6 @@ class CodexAdapter:
             "--stdio",
             "--enable",
             "multi_agent",
-            "--enable",
-            "multi_agent_v2",
             "-c",
             'approval_policy="never"',
             "-c",
